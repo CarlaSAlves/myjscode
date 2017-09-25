5d48a3d9cc4163ee2a548b53c1382fb1fb2223ab
Carla Alves <carla.soeiro@gmail.com>
2017-09-25 21:38:48 +0100

    Codecademy exercise Dog Years

---
+++ DogYears.js
@@ -0,0 +1,18 @@
+//Create a vaiable myAge equal to my age
+const myAge = 42;
+//Create variable earlyYears equal to 2 (the 2 human years)
+let earlyYears = 2;
+//Multiply earlyYears by 10.5 (the 2 first years from a dog life)
+earlyYears*=10.5;
+//Subtract 2 from MyAge variable and save it in laterYears variable
+let laterYears = myAge - 2;
+//Multiply laterYears by 4; calculate the dog years (use the multiplication assinment operator *=)
+laterYears*=4;
+//create a viraible myAgeInDogYears adding earlyYears to a laterYears
+myAgeInDogYears = earlyYears + laterYears;
+//create a variable whith my name
+let myName = 'Carla Alves';
+//the lower case string Medtod
+myName = myName.toLowerCase();
+//console.log string concatenation
+console.log('My name is ' + myName + '. I am '+ myAgeInDogYears + ' years old in dog years.');
