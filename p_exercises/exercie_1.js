16ce69b427a9b57b4243a400f742ba537a51fb8c
Carla Alves <carla.soeiro@gmail.com>
2017-10-09 17:42:08 +0100

    exercise_1

---
+++ p_exercises/exercie_1.js
@@ -0,0 +1,26 @@
+/*a) cria uma função que recebe um argumento name e que imprime para a consola 
+"Hello name": (sendo que name é o argumento que é passado à função no momento de 
+invocá-la)
+*/
+const helloName = (name) => {
+    return console.log("Hello " + name + "!");
+};
+
+//helloName("Carla");
+
+/*b) cria outra função que recebe os argumentos name e language. 
+Esta função imprime para a consola olá em PT, ES ou EN consoante os 
+argumentos que recebe*/
+
+const multyHello = (name, lenguage) => {
+    if (lenguage === "PT") {
+        return ("Olá " + name + "!");
+    };
+    if (lenguage === "ES") {
+        return ("Hola " + name + "!");
+    };
+    if (lenguage === "EN") {
+        return ("Hello " + name + "!");
+    };
+};
+console.log(multyHello("Prashanta", "EN"));
